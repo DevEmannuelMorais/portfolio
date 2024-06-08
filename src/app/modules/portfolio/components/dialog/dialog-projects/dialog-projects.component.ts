9f9bbcf452f0b54cd47fb8698b76f1edd426ca51
Emannuel Morais <emanuelmorais2015@gmail.com>
2024-06-08 18:01:11 -0300

    Concluindo o dialog dos projetos

--- src/app/modules/portfolio/components/dialog/dialog-projects/dialog-projects.component.ts
+++ src/app/modules/portfolio/components/dialog/dialog-projects/dialog-projects.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit, signal} from '@angular/core';
+import {IProjects} from "../../../interface/IProjects.interface";
+import {MAT_DIALOG_DATA, MatDialogContent, MatDialogRef, MatDialogTitle} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-dialog-projects',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogContent,
+    MatDialogTitle
+  ],
   templateUrl: './dialog-projects.component.html',
   styleUrl: './dialog-projects.component.scss'
 })
-export class DialogProjectsComponent {
+export class DialogProjectsComponent implements OnInit{
+  constructor(
+    private dialogRef: MatDialogRef<DialogProjectsComponent>,
+    @Inject(MAT_DIALOG_DATA) private _data: IProjects
+  ) {
+  }
+
+  public getData = signal<IProjects | null>(null);
+
+  ngOnInit(): void {
+    this.getData.set(this._data);
+  }
+
+  public closeModal() {
+   return  this.dialogRef.close();
+  }
+
 
 }
--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, signal} from '@angular/core';
 import {IProjects} from "../../interface/IProjects.interface";
 import {MatDialog, MatDialogModule} from "@angular/material/dialog";
 import {DialogProjectsComponent} from "../dialog/dialog-projects/dialog-projects.component";
+import {EDialogPanelClass} from "../../enum/EDialogPanelClass.enum";
 
 @Component({
   selector: 'app-projects',
@@ -19,7 +20,8 @@ export class ProjectsComponent {
       title: "Projetos em construção",
       width: "100",
       height: "51",
-      description: "Descrição do projeto 1",
+      description:
+        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p> <p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
       link: [
         {
         name: 'Conheça o Blog',
@@ -32,7 +34,7 @@ export class ProjectsComponent {
   public openDialog(data: IProjects) {
     this.#dialog.open(DialogProjectsComponent, {
       data,
-      panelClass: 'dialog-projects',
+      panelClass: EDialogPanelClass.PROJECTS,
     });
   }
 
